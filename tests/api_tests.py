09e0d4a5957e2d60c932c42b053ba5d250e7206d
Hussein Kabbout <hussein.kabbout@hotmail.com>
2021-04-01 17:51:14 +0200

    Update unittest WMS params and remove not needed unittest

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -25,21 +25,21 @@ class ApiTestCase(unittest.TestCase):
             access_token = create_access_token('test')
         return {'Authorization': 'Bearer {}'.format(access_token)}
 
-    def test_info_db(self):
+    def test_info_module(self):
         params = {
             'service': 'WMS',
             'request': 'GetFeatureInfo',
             'info_format': 'text/xml',
             'version': '1.3.0',
-            'layers': 'test_poly',
-            'i': 1280,
-            'j': 1024,
-            'height': 512,
-            'width': 640,
-            'bbox': "1200000,-4560000,1400000,-4540000",
-            'crs': 'EPSG:2056'
+            'layers': 'edit_points',
+            'i': 51,
+            'j': 51,
+            'height': 101,
+            'width': 101,
+            'bbox': "671639,5694018,1244689,6267068",
+            'crs': 'EPSG:3857'
         }
-        response = self.app.get('/somap?' + urlencode(params), headers=self.jwtHeader())
+        response = self.app.get('/qwc_demo?' + urlencode(params), headers=self.jwtHeader())
 
         doc = parseString(response.data)
         featureInfoResponses = doc.getElementsByTagName("GetFeatureInfoResponse")
@@ -49,32 +49,5 @@ class ApiTestCase(unittest.TestCase):
         features = layers[0].getElementsByTagName("Feature")
         self.assertEqual(len(features), 1)
         self.assertEqual(features[0].getAttribute("id"), "1")
-        htmlContents = features[0].getElementsByTagName("HtmlContent")
-        self.assertEqual(len(htmlContents), 1)
-
-    def test_info_module(self):
-        params = {
-            'service': 'WMS',
-            'request': 'GetFeatureInfo',
-            'info_format': 'text/xml',
-            'version': '1.3.0',
-            'layers': 'test_point',
-            'i': 1280,
-            'j': 1024,
-            'height': 512,
-            'width': 640,
-            'bbox': "1200000,-4560000,1400000,-4540000",
-            'crs': 'EPSG:2056'
-        }
-        response = self.app.get('/somap?' + urlencode(params), headers=self.jwtHeader())
-
-        doc = parseString(response.data)
-        featureInfoResponses = doc.getElementsByTagName("GetFeatureInfoResponse")
-        self.assertEqual(len(featureInfoResponses), 1)
-        layers = featureInfoResponses[0].getElementsByTagName("Layer")
-        self.assertEqual(len(layers), 1)
-        features = layers[0].getElementsByTagName("Feature")
-        self.assertEqual(len(features), 1)
-        self.assertEqual(features[0].getAttribute("id"), "123")
         htmlContents = features[0].getElementsByTagName("HtmlContent")
         self.assertEqual(len(htmlContents), 1)
